6f7c7210a44d8d32f85657fb63a026b78216f59b
Yasir Qumane <ST10473508@vcconnect.edu.za>
2026-05-09 16:36:54 +0200

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -120,4 +120,4 @@ class Choice(models.Model):
 # One choice could belong to multiple submissions
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+    choices = models.ManyToManyField(Choice)
